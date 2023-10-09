7b8c409899cec039a1d575f0248d0ddc2befe13b
El Mostafa Ramdi <93084739+Elmostafa01@users.noreply.github.com>
2023-10-09 23:43:50 +0100

    update components

---
+++ src/components/Map.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './Map.module.css'
+
+const Map = () => {
+  return (
+    <div className='mapContainer'>
+      Map
+    </div>
+  )
+}
+
+export default Map
